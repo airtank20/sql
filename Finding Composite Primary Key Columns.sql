e1fc25e4b1218732b38a9ff2c71fe3d6c57c2942
John Morehouse <john@jmorehouse.com>
2018-02-07 22:44:40 -0500

    more updates
    just cleaned up the code to make it more readable, aligned some stuff, added aliases, etc.

--- Finding Composite Primary Key Columns.sql
+++ Finding Composite Primary Key Columns.sql
@@ -1,25 +1,32 @@
 
 -- Let's get the columns of the Primary key into a CTE
-;WITH mycte AS (SELECT OBJECT_NAME(i2.object_id) AS 'TableName',STUFF(
-					(SELECT ',' + COL_NAME(ic.object_id,ic.column_ID) 
-					FROM sys.indexes i1
-						INNER JOIN sys.index_columns ic ON i1.object_id = ic.object_id AND i1.index_id = ic.index_id
-					WHERE i1.is_primary_key = 1
-						AND i1.object_id = i2.object_id	AND i1.index_id = i2.index_id
-					FOR XML PATH('')),1,1,'') AS PK
+;WITH mycte AS (SELECT SCHEMA_NAME(o.schema_id) as 'Schema'
+					, OBJECT_NAME(i2.object_id) AS 'TableName'
+					, STUFF(
+						(SELECT ',' + COL_NAME(ic.object_id,ic.column_ID) 
+						FROM sys.indexes i1
+							INNER JOIN sys.index_columns ic ON i1.object_id = ic.object_id AND i1.index_id = ic.index_id
+						WHERE i1.is_primary_key = 1
+							AND i1.object_id = i2.object_id	AND i1.index_id = i2.index_id
+						FOR XML PATH('')),1,1,'') AS PK
 FROM sys.indexes i2
 	INNER JOIN sys.objects o ON i2.object_id = o.object_id
 WHERE i2.is_primary_key = 1
 	AND o.type_desc = 'USER_TABLE'
 )
 -- Use this select to get the count, join to the CTE and get the column list
-SELECT OBJECT_NAME(i.object_id) AS 'TableName',COUNT(COL_NAME(ic.object_id,ic.column_id)) AS 'Primary_Key_Column_Count', MAX(mycte.PK) AS 'Primary_Key_Columns'
-	FROM sys.indexes i 
-		INNER JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
-		INNER JOIN sys.objects o ON i.object_id = o.object_ID
-		INNER JOIN mycte ON mycte.TableName = OBJECT_NAME(i.object_id)
-	WHERE i.is_primary_key = 1
+SELECT schema_name(o.schema_id) as 'Schema'
+	, object_name(i.object_id) AS 'TableName'
+	, count(COL_NAME(ic.object_id,ic.column_id)) AS 'Primary_Key_Column_Count'
+	, mycte.PK AS 'Primary_Key_Columns'
+FROM sys.indexes i 
+	INNER JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
+	INNER JOIN sys.objects o ON i.object_id = o.object_ID
+	INNER JOIN mycte ON mycte.TableName = OBJECT_NAME(i.object_id)
+WHERE i.is_primary_key = 1
 		AND o.type_desc = 'USER_TABLE'
-GROUP BY OBJECT_NAME(i.object_id)
+GROUP BY schema_name(o.schema_id)
+		, object_name(i.object_id)
+		, mycte.pk
 HAVING COUNT('Primay_Key_Column_Count') > 1
 ORDER BY 'TableName' ASC
