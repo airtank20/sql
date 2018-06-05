3d8c8a7bfae7bcfc477955cc83bb55ca8aeb6c66
John Morehouse <john@jmorehouse.com>
2018-06-05 16:20:00 -0400

    update
    added some additional parameters

--- Configure Alerts.sql
+++ Configure Alerts.sql
@@ -3,9 +3,17 @@
 
 
 DECLARE @operator VARCHAR(50) = 'SQLAdmins'
+DECLARE @notificationmethod TINYINT = 1 -- 1 email, 2 pager, 4 net send
 DECLARE @delay INT = 600 -- seconds
 DECLARE @alerts TABLE (alertName varchar(100), severity CHAR(5), messageid CHAR(5), processed bit DEFAULT(0))
 DECLARE @sql VARCHAR(1000)
+
+-- check first to see if operator exists
+IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysoperators WHERE name = @operator)
+	BEGIN
+		PRINT 'Operator does not exist'
+	END
+
 INSERT @alerts (alertName, severity, messageID)
 	select 	'017- Insufficient Resources', 17, 0 UNION
 	select 	'018- Nonfatal Internal Error', 18, 0 UNION
@@ -44,11 +52,25 @@ IF NOT EXISTS( SELECT 1 FROM msdb.dbo.sysnotifications sy
 GO'
 FROM @alerts
 
-IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysalerts WHERE name='017- Insufficient Resources')   
-	BEGIN    
-		EXEC msdb.dbo.sp_add_alert @name=N'017- Insufficient Resources',     @message_id=0    ,     @severity=17   ,     @enabled=1,     @delay_between_responses=600,     @include_event_description_in=2,     @job_id=N'00000000-0000-0000-0000-000000000000'   
+IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysalerts WHERE name='017- Insufficient Resources')
+   BEGIN    
+	EXEC msdb.dbo.sp_add_alert @name=N'017- Insufficient Resources',     
+		@message_id=0    ,     
+		@severity=17   ,     
+		@enabled=1,     
+		@delay_between_responses=600,     
+		@include_event_description_in=2,     
+		@job_id=N'00000000-0000-0000-0000-000000000000'   
 	END
-	GO   
-	IF NOT EXISTS( SELECT 1 FROM msdb.dbo.sysnotifications sy        INNER JOIN msdb.dbo.sysalerts sa ON sy.alert_id = sa.id        INNER JOIN msdb.dbo.sysoperators so ON sy.operator_id = so.id       WHERE so.name = 'SQLAdmins' AND        sa.name = '017- Insufficient Resources')   BEGIN    EXEC msdb.dbo.sp_add_notification @alert_name=N'017- Insufficient Resources', @operator_name=N'SQLAdmins', @notification_method = 1;   
+GO   
+IF NOT EXISTS( SELECT 1 FROM msdb.dbo.sysnotifications sy
+	INNER JOIN msdb.dbo.sysalerts sa ON sy.alert_id = sa.id        
+	INNER JOIN msdb.dbo.sysoperators so ON sy.operator_id = so.id       
+WHERE so.name = 'SQLAdmins' AND sa.name = '017- Insufficient Resources')
+	BEGIN    
+		EXEC msdb.dbo.sp_add_notification 
+			@alert_name=N'017- Insufficient Resources', 
+			@operator_name=N'SQLAdmins', 
+			@notification_method = 1;   
 	END  
-	GO
+GO
